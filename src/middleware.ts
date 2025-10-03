5ace556ecb79954ad3b0177640c9215fea6d1446
Otisium <kartikmhatre1812@gmail.com>
2025-10-03 19:53:22 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+hhhhhhimport { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
   "/",
