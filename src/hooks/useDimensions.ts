5556777eaac856011879337971a4ce26879c5521
Otisium <kartikmhatre1812@gmail.com>
2025-10-08 19:13:37 +0530

    Update useDimensions.ts

--- src/hooks/useDimensions.ts
+++ src/hooks/useDimensions.ts
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"; 
    
-export default function useDimensions(
+export default function useDimensions( 
   containerRef: React.RefObject<HTMLElement>,
 ) {
   const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
